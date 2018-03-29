da336838f452a9931f42a855c83e7c92ec7edee9
Bryce <bryce@flippedout.com.au>
2018-03-30 00:38:55 +0800

    Add project files.

---
+++ Muser-DBPopulator/Muser.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flippedout
+
+{
+
+    class Muser {
+
+        private string Location { get; set; }
+        private bool OnlineCollaboration { get; set; }
+        private List<String>[] Instruments { get; set; }
+        private List<String>[] Genres { get; set; }
+        private string Skill { get; set; }
+        private string EmailAddress { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private bool IsAdmin = false;
+        private bool IsHidden = false;
+        private bool IsValid = true;
+
+        private string Password { get; set; }
+        private string Salt { get; set; }
+
+        // Muser constructor
+        public Muser() {}
+
+		// Muser othershit
+	}
+}
